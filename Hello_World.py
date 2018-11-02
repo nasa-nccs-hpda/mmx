f6bc22b5b7a53814ce74db9a033a701db509eb9a
High Performance Data Analytics <10469195+nasa-nccs-hpda@users.noreply.github.com>
2018-11-02 17:34:03 -0400

    Hello World python script

---
+++ Hello_World.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+'''
+Created on Nov 2, 2018
+
+@author: nasa-nccs-hpda
+'''
+import time
+import sys
+
+class UserApp(object):
+
+    if __name__ == '__main__':
+
+         start_time = time.time()
+         elapsed_time = time.time() - start_time
+         print elapsed_time, " seconds wall time for Hello World script\n"
